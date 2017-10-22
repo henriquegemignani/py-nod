5e2cf1ddeaddfac01c52e62c61aea0c8a1bf61f5
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2017-10-22 16:12:51 -0200

    Fix C++ wrapper

--- py-nod/nod_wrap_util.cxx
+++ py-nod/nod_wrap_util.cxx
@@ -4,16 +4,50 @@ namespace nod_wrap {
 
 class PyObjectHolder {
 public:
-	PyObjectHolder(PyObject* the_obj) : obj_(the_obj) {
-		Py_XINCREF(the_obj);
+	PyObjectHolder(PyObject* the_obj)
+	: obj_(the_obj) {
+		increment();
 	}
 	~PyObjectHolder() {
-		Py_XDECREF(obj_);
+		decrement_and_clear();
+	}
+
+	PyObjectHolder(const PyObjectHolder& other)
+	: obj_(other.obj_) {
+		increment();
+	}
+
+	PyObjectHolder(PyObjectHolder&& other)
+	: obj_(other.obj_) {
+		other.clear();
+	}
+
+	PyObjectHolder& operator=(const PyObjectHolder& other) {
+		decrement_and_clear();
+		obj_ = other.obj_;
+		increment();
+		return *this;
+	}
+
+	PyObjectHolder& operator=(PyObjectHolder&& other) {
+		decrement_and_clear();
+		obj_ = other.obj_;
+		other.clear();
 	}
 
 	PyObject* obj() const { return obj_; }
 private:
 	PyObject* obj_;
+
+	void increment() const {
+		Py_XINCREF(obj_);
+	}
+	void decrement_and_clear() {
+		Py_CLEAR(obj_);
+	}
+	void clear() {
+		obj_ = nullptr;
+	}
 };
 
 std::function<void(const std::string&, float)> createProgressCallbackFunction(PyObject * obj, void (*callback)(PyObject *, const std::string&, float)) {
