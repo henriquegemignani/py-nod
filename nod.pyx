4d89164e0045d9d3b62c640e0d7a1b0a6a87a127
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2017-10-21 23:36:35 -0200

    Expose prettier names

--- nod.pyx
+++ nod.pyx
@@ -1,18 +1,15 @@
 from typing import Tuple, Optional
 
-from libc.stddef cimport wchar_t
-from libc.stdint cimport uint64_t
 from libcpp cimport bool as c_bool
 from libcpp.string cimport string
 from libcpp.memory cimport unique_ptr
-from libcpp.functional cimport function
 
 from nod_wrap cimport ExtractionContext as c_ExtractionContext, createProgressCallbackFunction, DiscBase as c_DiscBase, OpenDiscFromImage, SystemStringView, wstring
 
 cdef void invoke_callback_function(object callback, const string& a, float progress):
     callback(a.decode("utf-8"), progress)
 
-cdef class ExtractionContextWrapper:
+cdef class ExtractionContext:
     cdef c_ExtractionContext c_context
 
     def __cinit__(self):
@@ -29,29 +26,29 @@ cdef class ExtractionContextWrapper:
     def set_progress_callback(self, callback):
         self.c_context.progressCB = createProgressCallbackFunction(callback, invoke_callback_function)
 
-cdef class PartitionWrapper:
+cdef class Partition:
     cdef c_DiscBase.IPartition*c_partition
 
-    def extract_to_directory(self, path: str, context: ExtractionContextWrapper):
+    def extract_to_directory(self, path: str, context: ExtractionContext):
         self.c_partition.extractToDirectory(
             _str_to_system_string(path),
             context.c_context
         )
 
-cdef class DiscBaseWrapper:
+cdef class DiscBase:
     cdef unique_ptr[c_DiscBase] c_disc
 
     def get_data_partition(self):
         cdef c_DiscBase.IPartition*partition = self.c_disc.get().getDataPartition()
         if partition:
-            wrapper = PartitionWrapper()
+            wrapper = Partition()
             wrapper.c_partition = partition
             return wrapper
         else:
             return None
 
-def open_disc_from_image(path: str) -> Optional[Tuple[DiscBaseWrapper, bool]]:
-    disc = DiscBaseWrapper()
+def open_disc_from_image(path: str) -> Optional[Tuple[DiscBase, bool]]:
+    disc = DiscBase()
     cdef c_bool is_wii = True
     disc.c_disc = OpenDiscFromImage(_str_to_system_string(path).c_str(), is_wii)
 
