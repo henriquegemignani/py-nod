b44dff21bff66eddab6b78818b4eba9c4c0fa582
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2018-07-23 23:43:09 -0300

    v1.0.3

--- setup.py
+++ setup.py
@@ -146,7 +146,7 @@ with open("README.md") as readme_file:
 
 setup(
     name='nod',
-    version="1.0.2",
+    version="1.0.3",
     author='Henrique Gemignani',
     url='https://github.com/henriquegemignani/py-nod',
     description='Python bindings for the nod library.',
