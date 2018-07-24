ca4a2dcdf53c108c808fce8b5587333a940c6004
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2018-07-23 23:56:43 -0300

    Adding auditwheel repair

--- travis-scripts/build.sh
+++ travis-scripts/build.sh
@@ -5,7 +5,12 @@ set -e
 if [ -n "$PYTHON_VERSION" ]; then
     echo "Adding $PYTHON_VERSION to path"
     export PATH="/opt/python/${PYTHON_VERSION}/bin:$PATH"
+    python -m pip install git+https://github.com/wtolson/auditwheel@manylinux2010
 fi
 
 python -m pip install Cython
 python setup.py bdist_wheel
+
+if [ -n "$PYTHON_VERSION" ]; then
+    auditwheel repair dist/*.whl
+fi
