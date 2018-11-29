bfa0cdf2e76ae5f29ca24b7cadd0c5575f4ec759
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2018-11-29 00:54:16 -0200

    Use official auditwheel, but with non-stable version

--- travis-scripts/build.sh
+++ travis-scripts/build.sh
@@ -5,7 +5,7 @@ set -e
 if [ -n "$PYTHON_VERSION" ]; then
     echo "Adding $PYTHON_VERSION to path"
     export PATH="/opt/python/${PYTHON_VERSION}/bin:$PATH"
-    python -m pip install git+https://github.com/wtolson/auditwheel@manylinux2010
+    python -m pip install git+https://github.com/pypa/auditwheel
 fi
 
 python -m pip install Cython
