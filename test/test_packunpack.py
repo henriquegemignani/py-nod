937bdca34a670233c8ace965ca94c5c2cd8d0701
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2021-07-13 23:24:52 +0300

    Fix test

--- test/test_packunpack.py
+++ test/test_packunpack.py
@@ -19,10 +19,10 @@ def sha256_checksum(filename, block_size=65536):
     return sha256.hexdigest()
 
 
-@pytest.fixture(name="pack_sample_game")
-def _pack_sample_game(tmp_path):
+@pytest.fixture(name="pack_sample_game", scope="session")
+def _pack_sample_game(tmp_path_factory):
     filesystem_root = Path(__file__).parent.joinpath("sample_game")
-    image_out = tmp_path.joinpath("game.iso")
+    image_out = tmp_path_factory.mktemp("iso").joinpath("game.iso")
     disc_builder = nod.DiscBuilderGCN(os.fspath(image_out), fprogress_callback)
     disc_builder.build_from_directory(os.fspath(filesystem_root))
     return image_out
@@ -31,16 +31,17 @@ def _pack_sample_game(tmp_path):
 
 def test_packunpack(pack_sample_game):
     iso_hash = sha256_checksum(pack_sample_game)
-    assert iso_hash == '53a936817bb3fd6032b9496a7b5a55627e9d52dbf257cb2fcb927ff2c66ea950'
+    assert iso_hash == '0d98f9bf2c94051bec6145373c0b2c4baee0b35ccb066eaf354fd5a7b9324816'
 
 
 def test_build_and_check(pack_sample_game):
     result = nod.open_disc_from_image(os.fspath(pack_sample_game))
     assert result is not None
     assert not result[1]
-    disc = pack_sample_game[0]
+    disc = result[0]
 
-    assert disc.get_data_partition().files() == [
+    files = disc.get_data_partition().files()
+    assert sorted(files) == [
         "a.txt",
         "b.txt",
         "d/nested.txt",
